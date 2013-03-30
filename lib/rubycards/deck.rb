f415c19265c05f7a689d9552f8e962450ce878a2
Jordan Scales <scalesjordan@gmail.com>
2013-03-29 23:03:42 -0400

    Deck class

---
+++ lib/rubycards/deck.rb
@@ -0,0 +1,45 @@
+class Deck
+
+  include Enumerable
+
+  attr_reader :cards
+
+  RANKS = [*2..10, 'Jack', 'Queen', 'King', 'Ace']
+  SUITS = %w{ Clubs Diamonds Hearts Spades }
+
+  # initialize a deck of cards using the RANKS and SUITS constants
+  def initialize
+    @cards = []
+
+    RANKS.product(SUITS).each do |rank, suit|
+      @cards << Card.new(rank, suit)
+    end
+  end
+
+  # shuffle the deck
+  def shuffle!
+    @cards.shuffle!
+  end
+
+  # draw a card from the deck
+  def draw
+    @cards.shift
+  end
+
+  # determines if the deck is empty
+  def empty?
+    @cards.empty?
+  end
+
+  # Enumerable#each
+  def each(&block)
+    @cards.each do |card|
+      if block_given?
+        block.call card
+      else
+        yield card
+      end
+    end
+  end
+
+end
