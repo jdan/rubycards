ee8d1190d2228c26f8f81d8dd902a96186fd7061
Jordan Scales <scalesjordan@gmail.com>
2013-03-29 22:25:59 -0400

    safer hand initialization

--- lib/rubycards/hand.rb
+++ lib/rubycards/hand.rb
@@ -2,24 +2,34 @@ class Hand
 
   attr_reader :cards
 
+  # initialize with an array of cards
   def initialize(cards = [])
-    @cards = cards
+    @cards = []
+    cards.each do |card|
+      self << card
+    end
   end
 
+  # add a card if it is valid
   def add(card)
     @cards << card if card.instance_of? Card
   end
 
+  # alias array push (<<) to add method
   alias_method :<<, :add
 
+  # indexing
   def [](n)
     @cards[n]
   end
 
+  # sorting
   def sort!
     @cards.sort!
   end
 
+  # displaying the card icons
+  # using extensions/string.rb for String#next
   def to_s
     @cards.map(&:to_s).inject(:next)
   end
