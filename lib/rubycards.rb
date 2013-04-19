66dc48b01f3b2dbb0d4c7d690575f11e55d54ba1
Jordan Scales <scalesjordan@gmail.com>
2013-04-19 14:16:50 -0400

    removed legacy code

--- lib/rubycards.rb
+++ lib/rubycards.rb
@@ -5,9 +5,6 @@ require 'rubycards/card'
 require 'rubycards/deck'
 require 'rubycards/hand'
 
-ranks = [*2..10, 'jack', 'queen', 'king', 'ace']
-suits = %w{ clubs diamonds hearts spades }
-
 h = Hand.new
 d = Deck.new
 
