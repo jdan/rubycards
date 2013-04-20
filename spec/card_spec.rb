e0313b9b831befb7de715c1985d8f4cbb1a2358a
Joe Letizia <joe.letizia@gmail.com>
2013-04-20 18:24:36 -0400

    added tests for comparator

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -22,15 +22,51 @@ describe Card do
     end
   end
 
+  describe '#comparator' do
+      let(:king){Card.new('King','Clubs')}
+      let(:queen){Card.new('Queen','Clubs')}
+      let(:jack){Card.new('Jack','Clubs')}
+      let(:ace){Card.new('Ace','Clubs')}
+      let(:c2){Card.new(2,'Diamonds')}
+      let(:c2_heart){Card.new(2,'Hearts')}
+      let(:c4){Card.new(4,'Spades')}
+
+      it 'should reflect the correct rank when compared' do
+        king.should > queen
+        king.should > jack
+        king.should == king
+        king.should < ace
+
+        jack.should_not > queen
+        jack.should_not > ace
+        jack.should < queen
+
+        ace.should_not < queen
+        ace.should > c4
+
+        c2.should < c4
+        c2_heart.should == c2
+
+      end
+  end
+
   describe '#rank' do
     context 'face cards' do
       let(:king){Card.new('King','Clubs')}
 
-      it 'should return a long ranks' do
+      it 'should return a long rank' do
         king.rank.should == 'King'
       end
       it 'should return a short rank' do
         king.rank(true).should == 'K'
+      end
+    end
+
+    context 'numeric cards' do
+      let(:num){Card.new(10,'Diamonds')}
+
+      it 'should have the same long and short rank' do
+        num.rank.should == num.rank(true)
       end
     end
   end
