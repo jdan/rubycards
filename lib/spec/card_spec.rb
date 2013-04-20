97923d6815f0b4ad9e29d3ba64b07dd703eb1edc
Joe Letizia <joe.letizia@gmail.com>
2013-04-20 17:03:26 -0400

    testing framework files

---
+++ lib/spec/card_spec.rb
@@ -0,0 +1,5 @@
+require '../lib/card.rb'
+
+describe Card do
+  
+end
---
+++ lib/spec/factory.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :card do
+  end
+
+  factory :deck do
+  end
+
+  factory :hand do
+  end
+end
