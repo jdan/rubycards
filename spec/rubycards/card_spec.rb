43f00b50a10d4f5fad990c8fcb421e81c0e9170d
Jordan Scales <scalesjordan@gmail.com>
2013-04-27 22:22:12 -0400

    #12 - more airtight card spec

--- spec/rubycards/card_spec.rb
+++ spec/rubycards/card_spec.rb
@@ -20,56 +20,90 @@ describe Card do
         new_card = Card.new(3, 'Spades')
         new_card.rank.should == 3.to_s
         new_card.suit.should == 'Spades'
+
+        new_card = Card.new('Jack', 'Diamonds')
+        new_card.rank.should == 'Jack'
+        new_card.suit.should == 'Diamonds'
+
+        new_card = Card.new(7, 'Hearts')
+        new_card.rank.should == 7.to_s
+        new_card.suit.should == 'Hearts'
       end
     end
   end
 
   describe '#comparator' do
-      let(:king){Card.new('King','Clubs')}
-      let(:queen){Card.new('Queen','Clubs')}
-      let(:jack){Card.new('Jack','Clubs')}
-      let(:ace){Card.new('Ace','Clubs')}
-      let(:c2){Card.new(2,'Diamonds')}
-      let(:c2_heart){Card.new(2,'Hearts')}
-      let(:c4){Card.new(4,'Spades')}
+    let (:king) { Card.new('King','Clubs') }
+    let (:queen) { Card.new('Queen','Clubs') }
+    let (:jack) { Card.new('Jack','Clubs') }
+    let (:ace) { Card.new('Ace','Clubs') }
+    let (:c2) { Card.new(2,'Diamonds') }
+    let (:c2_heart) { Card.new(2,'Hearts') }
+    let (:c4) { Card.new(4,'Spades') }
 
-      it 'should reflect the correct rank when compared' do
-        king.should > queen
-        king.should > jack
-        king.should == king
-        king.should < ace
+    it 'should reflect the correct rank when compared' do
+      king.should > queen
+      king.should > jack
+      king.should == king
+      king.should < ace
 
-        jack.should_not > queen
-        jack.should_not > ace
-        jack.should < queen
+      jack.should_not > queen
+      jack.should_not > ace
+      jack.should < queen
 
-        ace.should_not < queen
-        ace.should > c4
+      ace.should_not < queen
+      ace.should > c4
 
-        c2.should < c4
-        c2_heart.should == c2
-
-      end
+      c2.should < c4
+      c2_heart.should == c2
+    end
   end
 
   describe '#rank' do
     context 'face cards' do
-      let(:king){Card.new('King','Clubs')}
+      let (:king) { Card.new('King', 'Clubs') }
 
       it 'should return a long rank' do
         king.rank.should == 'King'
       end
+
       it 'should return a short rank' do
         king.rank(true).should == 'K'
       end
     end
 
     context 'numeric cards' do
-      let(:num){Card.new(10,'Diamonds')}
+      let (:num) { Card.new(10, 'Diamonds') }
 
       it 'should have the same long and short rank' do
         num.rank.should == num.rank(true)
       end
     end
   end
+
+  describe '#display' do
+    BORDER_COUNT = 18 # the number of unicode characters on the border
+    RANKS = [*2..10, 'Jack', 'Queen', 'King', 'Ace']
+
+    it 'should have the correct number of glyps' do
+      RANKS.each do |rank|
+        card = Card.new(rank, 'hearts')   # rspec doesn't play nice with dark cards
+
+        if (2..10).include? card.rank.to_i
+          card.to_s.scan(/[^\x00-\x7F]/).count.should == card.rank.to_i + BORDER_COUNT
+        else
+          card.to_s.scan(/[^\x00-\x7F]/).count.should == 2 + BORDER_COUNT
+        end
+      end
+    end
+
+    it 'should display #inspect the same as #short' do
+      RANKS.each do |rank|
+        card = Card.new(rank, 'diamonds')
+
+        card.inspect.should == card.short
+      end
+    end
+  end
+
 end
